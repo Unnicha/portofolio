410b96d39e2eb23ca375ec64df5c307321a86f26
Khansa <unnicha23@gmail.com>
2021-08-16 23:09:44 +0700

    intro : basic view & routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('judul', 'About Me')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1>About Me</h1>
+				<h4>let me introduce myself</h4>
+			</div>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('judul', 'Web Khansa')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1>Selamat Datang</h1>
+				<h4>di Website Portofolio Khansa ^^</h4>
+			</div>
+		</div>
+	</div>
+@endsection
